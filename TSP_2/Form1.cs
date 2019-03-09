8f9e018a90fd408ff73262b7cf0b425603b98d9d
Luzyanin Alexey <261-96@mail.ru>
2019-03-09 19:00:25 +0700

    Добавьте файлы проекта.

---
+++ TSP_2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace TSP_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1() => InitializeComponent();
+
+        private void Start_Button_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            var result = new Simulation().Simulate(Convert.ToDouble(lyambdaTextBox.Text), Convert.ToDouble(timeTextBox.Text));
+
+            var a = result.GetEnumerator();
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                chart1.Series[0].Points.AddXY(a.Current.time, Convert.ToDouble(a.Current.N));
+                a.MoveNext();
+            }
+        }
+    }
+}
---
+++ TSP_2/Simulation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSP_2
+{
+    struct Statistic
+    {
+        public double time;
+        public int N;
+
+        public Statistic(double time, int N)
+        {
+            this.time = time;
+            this.N = N;
+        }
+    }
+
+    class Simulation
+    {
+        public List<Statistic> Simulate(double l, double endTime)
+        {
+            double r, tau, lyambda = l / 24, t = 0.0;
+            int N = 0;
+            Random random = new Random();
+            List<Statistic> statistics = new List<Statistic>();
+            statistics.Add(new Statistic(0.0, 0));
+
+            while (t <= endTime)
+            {
+                r = random.NextDouble();
+                tau = (-1 / lyambda) * Math.Log(r);
+                t += tau;
+                N++;
+                statistics.Add(new Statistic(t, N));
+            }
+            return statistics;
+        }
+    }
+}
